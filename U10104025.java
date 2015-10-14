3a169a24a3b7577f9c97f200c6c8767300865e1a
LZong <lzong.tw@gmail.com>
2015-10-15 02:10:13 +0800

    Create U10104025.java

---
+++ U10104025.java
@@ -0,0 +1,10 @@
+import java.time.*; //Import class: java.time.*   '*' implies every sub class of java.time
+
+public class U10104025{
+    public static void main(String[] args){
+    
+	LocalDate time=LocalDate.now(); // Create a LocalTime object that use constructor "now"
+
+    System.out.println(time); //Print it
+    }	
+}
